8962ce38a9ff6f756c6b621850802ef2ba31fda5
Matthew McCord <mattm.eternal@gmail.com>
2021-02-16 23:38:12 -0800

    RequestManagerTests

--- LoadGeneratorTests/RequestsManagerTests.cs
+++ LoadGeneratorTests/RequestsManagerTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LoadGenerator_Swaroop_Project;
+using System.Net;
+using System.Net.Http;
 
 namespace LoadGeneratorTests
 {
@@ -7,8 +9,22 @@ namespace LoadGeneratorTests
     public class RequestManagerTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_EnqueueNewRequest()
         {
+            MockHttpClient mockHttp = new MockHttpClient();
+
+            mockHttp.PrepareGetAsyncResponse(
+                new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK
+                }
+            );
+
+            ProgramClient client = new ProgramClient("test/url", mockHttp);
+            RequestsManager manager = new RequestsManager(client);
+
+            manager.EnqueueNewRequest();
+            Assert.AreEqual(1, manager.TotalActiveRequests());
         }
     }
 }
