70b053c2852605990d17a69af2745d7156c65d56
Matthew McCord <mattm.eternal@gmail.com>
2021-02-16 23:38:44 -0800

    MockHttpclient for testing

---
+++ LoadGeneratorTests/MockHttpClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LoadGenerator_Swaroop_Project;
+using System.Net.Http;
+using System;
+
+namespace LoadGeneratorTests
+{
+    interface IMockHttpClient : IHttpClient
+    {
+        void PrepareGetAsyncResponse(HttpResponseMessage response);
+    }
+
+    class MockHttpClient : IHttpClient, IDisposable
+    {
+        private List<HttpResponseMessage> preparedResponses = new List<HttpResponseMessage>();
+
+        public void PrepareGetAsyncResponse(HttpResponseMessage response)
+        {
+            preparedResponses.Insert(0, response);
+        }
+
+        public void Dispose()
+        {}
+
+        public async Task<HttpResponseMessage> GetAsync(string uri)
+        {
+            HttpResponseMessage response = preparedResponses[0];
+            preparedResponses.RemoveAt(0);
+
+            return await Task.Run(() => response);
+        }
+    }
+}
